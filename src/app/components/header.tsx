384a15e7bd47af01ddcb89f85630d1f79e092dbc
hinanish <167354026+hinanish@users.noreply.github.com>
2024-11-05 20:38:05 +0500

    first

---
+++ src/app/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+function Header() {
+  return <div className="bg-[#A29875] h-auto w-full flex hover:bg-[#978235]">  <span className="text-[#FFFFFF] p-2 m-[4px] font-extrabold font-sans text-[33px] flex"> Purity 00106157 </span>
+  <div className="flex items-center bg-white rounded-full px-[10px] py-[5px] w-[50%] ml-72 h-8 mt-6">
+        <input
+          type="text"
+          placeholder="Search for Silver Jewellery ,Gold Jewellery, Diamond Jewellery,Antique Jewellery and more..."
+          className="w-full bg-transparent focus:outline-none text-gray-600 placeholder-gray-500"
+       /> </div></div>
+
+}  
+
+export default Header;
